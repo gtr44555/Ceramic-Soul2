ad5b7f789646d9e1576f5b1462dfb92b3d040234
Sofiya <sofiakobylanskaia@gmail.com>
2026-08-09 18:09:12 +0300

    Initial commit2

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/Ceramic-Soul2/",
+
   build: {
     rollupOptions: {
       input: {
